75b803cd2b90cb7e7cc3fce493564cb57f9e484d
Sniper10754 <Sniper10754@users.noreply.github.com>
2022-10-09 20:08:32 +0200

    Organize code, create setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,11 @@
 from setuptools import find_packages, setup
 
+setup(
+    name="ServerOcelot",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'serverocelot = mypkg.mymodule:some_func',
+        ]
+    }
+)
